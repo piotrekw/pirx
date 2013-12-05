1421dd89b74bf753cf0b52a5e6fe200d221922b5
Piotrek Wasilewski <wasilewski.piotrek@gmail.com>
2013-12-05 23:40:43 +0100

    Fix 'path' function: use main's file as project root

--- pirx/utils.py
+++ pirx/utils.py
@@ -5,6 +5,7 @@ def setting(name):
     return name.upper()
 
 def path(subpath):
-    project_root = os.path.dirname(os.path.realpath(__file__))
+    import __main__
+    project_root = os.path.dirname(os.path.realpath(__main__.__file__))
     return os.path.join(project_root, subpath)
 
