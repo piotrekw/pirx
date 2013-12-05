29fbae121e9883584e399749c693e27a1b5f9bdc
Piotrek Wasilewski <wasilewski.piotrek@gmail.com>
2013-12-05 02:01:13 +0100

    Add 'arg' check

--- pirx/checks.py
+++ pirx/checks.py
@@ -1,7 +1,17 @@
 #!/usr/bin/env python
 import socket
+import sys
 
 
 def host(name):
     return socket.gethostname() == name
+
+def arg(name, value=None):
+    args = [arg.split('=') for arg in sys.argv[1:]]
+    for arg in args:
+        if arg[0].lsplit('--') == name:
+            if len(arg) > 1:
+                return arg[1] == value
+            else:
+                return True
 
