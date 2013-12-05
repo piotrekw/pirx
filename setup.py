57bfd23957bdd535b5ae21ed1df3ff25dd75a8bd
Piotrek Wasilewski <wasilewski.piotrek@gmail.com>
2013-12-05 01:13:55 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='pirx',
+    version='0.1',
+    author='Piotr Wasilewski',
+    author_email='wasilewski.piotrek@gmail.com',
+    description='Django settings builder',
+    license='MIT',
+    keywords='django settings build builder',
+    url='https://github.com/piotrekw/pirx',
+    scripts=['scripts/pirx-build.py'],
+    packages=['pirx']
+    )
+
