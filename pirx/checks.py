e0cd0f9a14ac354f19c4e91367ac75b34d58ae8e
Piotrek Wasilewski <wasilewski.piotrek@gmail.com>
2013-12-30 23:40:45 +0100

    Set docstrings for check functions

--- pirx/checks.py
+++ pirx/checks.py
@@ -4,9 +4,14 @@ import sys
 
 
 def host(name):
+    """Check if host name is equal to the given name"""
     return socket.gethostname() == name
 
 def arg(name, expected_value=None):
+    """
+    Check if command-line argument with a given name was passed and if it has
+    the expected value.
+    """
     args = [
         arg.split('=') for arg in sys.argv[1:] if '=' in arg else (arg, None)
     ]
