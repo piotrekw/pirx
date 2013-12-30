670a5ffe8de9f21fbb2fe65e72e006d143bfa8e3
Piotrek Wasilewski <wasilewski.piotrek@gmail.com>
2013-12-30 16:38:53 +0100

    Set docstring for "path" function

--- pirx/utils.py
+++ pirx/utils.py
@@ -2,6 +2,7 @@ import os
 
 
 def path(*p):
+    """Return full path of a directory inside project's root"""
     import __main__
     project_root = os.path.dirname(os.path.realpath(__main__.__file__))
     return os.path.join(project_root, *p)
