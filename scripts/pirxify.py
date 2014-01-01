da66b1f2cae2b0505b3df87e1b92fecd2755be8f
Piotrek Wasilewski <wasilewski.piotrek@gmail.com>
2014-01-01 15:51:24 +0100

    Print pirxify's output to stdout instead of file

--- scripts/pirxify.py
+++ scripts/pirxify.py
@@ -11,39 +11,35 @@ import sys
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('in_path')
-    parser.add_argument('out_path')
     args = parser.parse_args()
 
-    in_path, out_path = args.in_path, args.out_path
+    if not os.path.exists(args.in_path):
+        sys.stderr.write('File does not exist: %s\n' % args.in_path)
+        sys.exit(1)
 
-    for path in [in_path, out_path]:
-        if not os.path.exists(path):
-            sys.stdout.write('File does not exist: %s\n' % path)
-            sys.exit(1)
-
-    mod = imp.load_source('settings', in_path)
+    mod = imp.load_source('settings', args.in_path)
     names = []
     for name, value in inspect.getmembers(mod):
         if name == name.upper():
             names.append(name)
 
-    sys.stdout.write(', '.join(names) + '\n')
+    sys.stderr.write(', '.join(names) + '\n')
     while True:
-        remove = raw_input('Press Enter or type name you want to remove: ')
+        sys.stderr.write('Press Enter or type name you want to remove: ')
+        remove = raw_input()
         if remove:
             if remove in names:
                 names.remove(remove)
-                sys.stdout.write(', '.join(names) + '\n')
+                sys.stderr.write(', '.join(names) + '\n')
             else:
-                sys.stdout.write('Name not in the list\n')
+                sys.stderr.write('Name not in the list\n')
         else:
             break
 
-    output = open(out_path, 'w')
-    output.write('from pirx import Settings\n\n\n')
-    output.write('settings = Settings()\n\n')
+    sys.stdout.write('from pirx import Settings\n\n\n')
+    sys.stdout.write('settings = Settings()\n\n')
     for name in names:
         value = pprint.pformat(getattr(mod, name))
-        output.write('settings.%s = %s\n' % (name.lower(), value))
-    output.write('\nprint settings')
+        sys.stdout.write('settings.%s = %s\n' % (name.lower(), value))
+    sys.stdout.write('\nprint settings')
 
